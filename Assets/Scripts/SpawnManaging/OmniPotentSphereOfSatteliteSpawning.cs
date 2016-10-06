2f534c7477c4e0680438fe87397ed56bc323f390
Loefvet <lovet1991@hotmail.com>
2016-10-06 12:50:12 +0200

    created test scene for spawning satellites

---
+++ Assets/Scripts/SpawnManaging/OmniPotentSphereOfSatteliteSpawning.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OmniPotentSphereOfSatteliteSpawning : MonoBehaviour {
+
+	private SpawnManager SpawnManager;
+	private int spawnTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+				spawnManager = SpawnManager.getInstance();
+	}
+
+	// Update is called once per frame
+	void Update () {
+			spawnTimer++;
+			if(spawnTimer == 100)
+			{
+				spawnManager.Spawnobject();
+				spawnTimer = 0;
+			}
+	}
+}
