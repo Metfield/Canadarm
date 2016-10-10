d490edaeea7bd22dd1bfc4b410aa099b1185a5bf
Emil Åhsberg <Emil hsberg>
2016-10-10 23:45:02 +0200

    added new way of calculating spawnlocations, now using polara coordinates. Primary testing looks promising

--- Assets/Scripts/SpawnManaging/SpawnManager.cs
+++ Assets/Scripts/SpawnManaging/SpawnManager.cs
@@ -50,31 +50,31 @@ public class SpawnManager : MonoBehaviour
         Vector3 shipPos = ship.transform.position;
         Vector3 spawnPos;
         
-        while (true)
-        {
             //Vector3 spawnPos = new Vector3(Random.Range(shipPos.x + MinSpawnDistance, shipPos.x + MaxSpawnDistance) * (Random.Range(0,2) * 2 -1),
             //                                Random.Range(shipPos.y + MinSpawnDistance, shipPos.y + MaxSpawnDistance) * (Random.Range(0,2) * 2 -1),
             //                                Random.Range(shipPos.z + MinSpawnDistance, shipPos.z + MaxSpawnDistance) * (Random.Range(0,2) * 2 -1));
 
-            spawnPos = new Vector3(Random.Range(shipPos.x - MaxSpawnDistance, shipPos.x + MaxSpawnDistance),
-                                   Random.Range(shipPos.y - MaxSpawnDistance, shipPos.y + MaxSpawnDistance),
-                                   Random.Range(shipPos.z - MaxSpawnDistance, shipPos.z + MaxSpawnDistance));
+            //spawnPos = new Vector3(Random.Range(shipPos.x - MaxSpawnDistance, shipPos.x + MaxSpawnDistance),
+            //                       Random.Range(shipPos.y - MaxSpawnDistance, shipPos.y + MaxSpawnDistance),
+            //                       Random.Range(shipPos.z - MaxSpawnDistance, shipPos.z + MaxSpawnDistance));
 
             //Vector3 spawnPos = new Vector3(Random.Range(shipPos.x + MinSpawnDistance, shipPos.x + MaxSpawnDistance),
             //                                 Random.Range(shipPos.y + MinSpawnDistance, shipPos.y + MaxSpawnDistance),
             //                                 Random.Range(shipPos.z + MinSpawnDistance, shipPos.z + MaxSpawnDistance));
 
 
-        if (Vector3.Distance(shipPos, spawnPos) > MinSpawnDistance)
-            {
-                break;
-            }
-        }
+            // generates a random 3D point in Polar coordiantes
+
+            int radius = Random.Range(MinSpawnDistance, MaxSpawnDistance);
+            float alpha = Random.Range(0, 2*Mathf.PI);
+            float beta = Random.Range(-Mathf.PI/2, Mathf.PI/2);
 
 
-
-
-
+            // Converts Polar coordinates to cartesian.
+            //TODO separate into new method
+            spawnPos = new Vector3(radius * Mathf.Cos(alpha) * Mathf.Cos(beta) + shipPos.x,
+                                   radius * Mathf.Sin(beta) + shipPos.y,
+                                   radius * Mathf.Sin(alpha) * Mathf.Cos(beta) + shipPos.z); 
         return spawnPos;
 	}
 }
