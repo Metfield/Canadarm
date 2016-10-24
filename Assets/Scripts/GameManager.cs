f689abe56115c205b218f02920f0a912bd792678
E-man <ebatis@outlook.com>
2016-10-24 02:07:53 +0200

    * Added timer text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,9 +20,13 @@ public class GameManager : MonoBehaviour
     private Text endGameText;
 
     [SerializeField]
+    private Text timer;
+
+    [SerializeField]
     private int playTimeInSeconds;
 
     private float gameTime;
+    private float timeLeft;
 
     private int x = 0;
 
@@ -51,10 +55,7 @@ public class GameManager : MonoBehaviour
         // InitSatellites();
         
         scoreText.gameObject.SetActive(false);        
-        endGameText.gameObject.SetActive(false);
-
-        scoreText.text = "SCORE: " + score;
-        scoreText.gameObject.SetActive(true);
+        endGameText.gameObject.SetActive(false);        
     }
 
     public int getMyBalls()
@@ -68,6 +69,14 @@ public class GameManager : MonoBehaviour
         timerStarted = true;
         gameStarted = true;
         gameTime = Time.time;
+
+        timeLeft = playTimeInSeconds;
+
+        scoreText.text = "SCORE: " + score;
+        scoreText.gameObject.SetActive(true);
+
+        timer.text = "TIME LEFT: " + playTimeInSeconds;
+        timer.gameObject.SetActive(true);
     }
 
     public bool IsGameOver()
@@ -119,6 +128,15 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(timerStarted && !gameOver)
+        {
+            timeLeft -= Time.deltaTime;
+            timer.text = "TIME LEFT: " + (int)timeLeft;
+
+            if (timeLeft <= 10)
+                timer.color = new Color(0.94f, 0.56f, 0.56f);
+        }
+
         if (x == 100)
         {
             //Debug.Log("tick tock " + (Time.time - gameTime) + " " + playTimeInSeconds);
@@ -134,7 +152,7 @@ public class GameManager : MonoBehaviour
             x++;
 
             scoreText.text = "SCORE: " + score;
-            scoreText.gameObject.SetActive(true);
+            scoreText.gameObject.SetActive(true);            
         }
 
         if(gameOver)
